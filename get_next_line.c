9fb41e5ade874c77a92515dd8fa4e6f5693089be
Elisée Sydney Adjiguidi <eliseesydneyadjiguidi@Pr-Stein.local>
2021-10-08 22:51:53 +0200

    test

--- get_next_line.c
+++ get_next_line.c
@@ -16,58 +16,73 @@ static char	*find_return(char **str)
 {
 	int		i;
 	char	*s1;
-	char *tmp;
+	char 	*tmp;
 
 	i = 0;
 	while ((*str)[i] != '\n' && (*str)[i] != '\0')
 		i++;
-	// if ((*str)[i] != '\n')
-	s1 = ft_substr(*str, 0, i + 1);
-	// else if ((*str)[i] != '\0')
-	// 	s1 = ft_substr(*str, 0, i);
-
+	s1 = ft_substr(*str, 0, i);
 	if ((*str)[i] == '\n')
 		tmp = ft_strdup(&(*str)[i + 1]);
-	// else if ((*str)[i] == '\0')
-	// {
-	// 	free(*str);
-	// 	free(s1);
-	// 	return (NULL);
-	// }
 	else
 		tmp = NULL;
 	free(*str);
 	*str = tmp;
-	if (*str == NULL)
-	{
-		if(s1)
-			free(s1);
-		free(*str);
-		return (NULL);
-	}
+	// if (*str == NULL)
+	// {
+	// 	// if(s1)
+	// 	// 	free(s1);
+	// 	// free(*str);
+	// 	return (NULL);
+	// }
 	return (s1);
 }
 
-static int	find_line(int fd, char **line, char **buf)
+// static int	find_line(int fd, char **line, char **buf)
+// {
+// 	int		i;
+// 	char	*str;
+//
+// 	i = 1;
+// 	while (i > 0)
+// 	{
+// 		i = read(fd, *buf, BUFFER_SIZE);
+// 		buf[0][i] = '\0';
+// 		if (!*line || !**line)
+// 			str = ft_strdup(*buf);
+// 		else
+// 		{
+// 			str = ft_strjoin(*line, *buf);
+// 		free(*line);
+// 		*line = str;
+// 		if (ft_check (*line, '\n') || ft_check (*line, '\0'))
+// 			break ;
+// 	}
+// 	free (*buf);
+// 	return (i);
+// }
+
+int	find_line(int fd, char **str, char **buf)
 {
-	int		i;
-	char	*str;
+	int i;
+	char *tmp;
 
 	i = 1;
 	while (i > 0)
 	{
 		i = read(fd, *buf, BUFFER_SIZE);
-		buf[0][i] = '\0';
-		if (!*line || !**line)
-			str = ft_strdup(*buf);
+		*buf[i] = '\0';
+		if (!*str || !**str)
+			*str = ft_strdup(*buf);
 		else
-			str = ft_strjoin(*line, *buf);
-		free(*line);
-		*line = str;
-		if (ft_check (*line, '\n') || ft_check (*line, '\0'))
+		{
+			tmp = ft_strjoin(*str, *buf);
+			free(*str);
+			*str = tmp;
+		}
+		if (ft_check (*str, '\n'))
 			break ;
 	}
-	free (*buf);
 	return (i);
 }
 
@@ -83,10 +98,7 @@ char	*get_next_line(int fd)
 	if (!buf)
 		return (NULL);
 	i = find_line(fd, &str, &buf);
+	free (buf);
 	// if (i < 0)
-	// {
-	// 	free(str);
-	// 	return (NULL);
-	// }
 	return (find_return(&str));
 }
